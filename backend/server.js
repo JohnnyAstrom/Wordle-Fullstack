26740920dd0eb3bf714a859a8c655faeab842017
JohnnyAstrom <johnny.astrom@hotmail.com>
2025-04-01 21:34:55 +0200

    fetch and display random word from backend on button click

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import gameRouter from './routes/game.js';
+import cors from 'cors';
 
 const app = express();
 const PORT = 5080;
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/api/game', gameRouter);
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,29 @@
+import { useState } from 'react';
+
 function App() {
+  const [word, setWord] = useState(null);
+  
+  async function fetchWord() {
+    try {
+      const response = await fetch('http://localhost:5080/api/game/start?length=5&unique=false');
+      const data = await response.json();
+      setWord(data.word);
+    }
+    catch (error) {
+      console.error('Could not fetch word:', error);
+    }
+  }
+    
   return (
     <div>
       <h1>Wordle-spelet</h1>
+      <button onClick={fetchWord}>Starta nytt spel</button>
+
+      {word && (
+        <p>
+          <strong>Hämtat ord: </strong> {word}
+        </p>
+      )}
     </div>
   );
 }
